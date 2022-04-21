0e50320378297da12b8888cb27c12daefc7dc541
Masaru <hittec9609@icloud.com>
2022-04-21 16:00:33 +0900

    2022.04.21

---
+++ PythonProgrammingA/2022.04.21/break.py
@@ -0,0 +1,26 @@
+# num = 1
+
+# while True:
+#   print(num)
+#   num += 1
+# この時点までは1ずつ増えるだけの無限ループ
+
+# numがその数字になったら処理を終了する
+  # if num == 1000:
+  #   break
+
+# 条件に当てはまった時に次の処理へ送る
+# while num <= 10:
+#   num += 1
+#   if num % 2 == 0:
+#     continue
+#   print(num)
+
+
+# whileの中身を書きたくない時にpassと書く, 後で書きたい時などに利用する
+num = 1
+
+while num <= 10:
+  pass
+
+print("aaa")
