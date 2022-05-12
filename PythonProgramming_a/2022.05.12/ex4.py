a043fef6ff2a76a1a80611a423d89b40704dac4d
Masaru <hittec9609@icloud.com>
2022-05-12 14:49:52 +0900

    2h

--- PythonProgramming_a/2022.05.12/ex4.py
+++ PythonProgramming_a/2022.05.12/ex4.py
@@ -3,14 +3,16 @@
 #
 # キー「英単語」と値「０または１」のペアである、辞書型のデータ「ENG_DIC」に対して、
 # 値が０の場合は「(英単語)は動物です」、値が１の場合は「 (英単語)は植物です」と出力する。
-# 出力例： 
+# 出力例：
 # dogは動物です
 # bananaは植物です
 
 # 英単語辞書
 ENG_DIC = {'dolphin': 0, 'cherry': 1, 'bamboo': 1, 'violet': 1, 'wolf': 0, 'giraffe': 0, 'lotus': 1, 'gnu': 0, 'lion': 0, 'dandelion': 1}
 
-
-
-
+for eng, kind in ENG_DIC.items():
+  if kind == 0:
+    print(str(eng) + ' は動物です\n')
+  else:
+    print(str(eng) + ' は植物です\n')
 
--- PythonProgramming_a/2022.05.12/set1.py
+++ PythonProgramming_a/2022.05.12/set1.py
@@ -2,15 +2,15 @@
 colors = {'red', 'orange', 'yellow'}            # 色のリスト
 fruits = {'apple', 'grape', 'orange', 'peach'}  # 果物のリスト
 
-# 和：どちらかにあるもの
-
+# 和：足し合わせて重複しているものは1つにする
+# |(パイプ)を使用
+print(colors | fruits)
 
 # 差：colorsだけにあるもの
-
+print(colors - fruits)
 
 # 積：両方にあるもの
-
+print(colors & fruits)
 
 # 対称差：どちらかにだけあるもの
-
-
+print(colors ^ fruits)
