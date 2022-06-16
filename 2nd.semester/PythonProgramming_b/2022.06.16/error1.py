acd4276b0f872842b59dc21d1286b04fb18ad702
Masaru <hittec9609@icloud.com>
2022-06-16 16:09:23 +0900

    8h

---
+++ 2nd.semester/PythonProgramming_b/2022.06.16/error1.py
@@ -0,0 +1,19 @@
+# a = 123
+
+# for i in range(a)
+#   print(i)
+
+# abd = "123"
+# print(int("abb"))
+
+# a = [100, 200, 300]
+# for cnt in range (-10, 10, 2):
+#   print(a[0] // cnt)
+
+# list_a = [1, 2, 3]
+
+# print(list_a)
+# print(list_a[3])
+# print(list_a[0:100])
+# print(x)
+
