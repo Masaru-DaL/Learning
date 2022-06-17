5886da016351b3cc8926af23e99c98f17d9284bd
Masaru <hittec9609@icloud.com>
2022-06-17 13:22:12 +0900

    5h

--- 2nd.semester/GameProgramming_a/2022.06.17/ex5.py
+++ 2nd.semester/GameProgramming_a/2022.06.17/ex5.py
@@ -4,4 +4,12 @@
 # 文字のフォーマットを用いて、小数点以下２ケタまで表示してください。
 # 何度も繰り返し実行して、いろんな数になることを確認しましょう。
 
+import random
+
+result = 1 + random.random()
+print(f"{result:.2f}")
+
+result1 = random.uniform(1,2)
+# print(f"{result1:.2f}")
+print(f"{result1:.2f}")
 
