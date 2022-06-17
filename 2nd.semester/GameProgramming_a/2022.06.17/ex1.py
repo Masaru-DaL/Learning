381cab6a9eef4f0f9e47a5abc8de97d1623cb5c1
Masaru <hittec9609@icloud.com>
2022-06-17 09:38:12 +0900

    1h

--- 2nd.semester/GameProgramming_a/2022.06.17/ex1.py
+++ 2nd.semester/GameProgramming_a/2022.06.17/ex1.py
@@ -1,6 +1,9 @@
 # 演習１
 
 # 定数（変わらない数字）１つ
+from email.charset import BASE64
+
+
 BASE_NUMBER = 25
 
 # 整数を入力してもらい、num に代入
@@ -9,8 +12,8 @@ num = int(input("整数を入力してください："))
 # ※以下のプログラム部分を書いて下さい
 
 # ５回繰り返す
-
+for i in range(5):
     # num に BASE_NUMBER を加算する
-    
+    num += BASE_NUMBER
     # numを出力する
-    
+    print(num)
