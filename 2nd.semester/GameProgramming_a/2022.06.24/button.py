49f7ce3fabef2dd71d56cb5e9109543b48f03745
Masaru <hittec9609@icloud.com>
2022-06-24 10:55:12 +0900

    3h

--- 2nd.semester/GameProgramming_a/2022.06.24/button.py
+++ 2nd.semester/GameProgramming_a/2022.06.24/button.py
@@ -1,13 +1,24 @@
-import tkinter                  # tkinterモジュールのインポート
+import tkinter          # tkinterモジュールのインポート
+
+def click_button():
+  my_button["text"] = "押してしまいましたね..."
+
+
 # ウィンドウオブジェクトの作成～サイズの指定
 root = tkinter.Tk()
 root.title("ボタン表示")
 root.geometry("800x600")
 
 # ボタンの作成
-
+my_button = tkinter.Button(
+  root, text = "押すなよ押すなよ！",
+  font = ("Arial Black", 32),
+  fg = "darkblue", bg = "lightblue",
+  command=click_button
+)
 
 # ボタンの配置
+my_button.place(x=350, y=300)
 
 # ウィンドウの表示
 root.mainloop()
