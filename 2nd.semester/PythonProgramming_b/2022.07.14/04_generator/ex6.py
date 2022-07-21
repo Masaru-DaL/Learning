6687a272aa3b53dcb9547a6d24ac004d72fcfe67
Masaru <hittec9609@icloud.com>
2022-07-21 13:54:01 +0900

    5h

--- 2nd.semester/PythonProgramming_b/2022.07.14/04_generator/ex6.py
+++ 2nd.semester/PythonProgramming_b/2022.07.14/04_generator/ex6.py
@@ -5,20 +5,26 @@
 # インスタンス作成時には、相手のチーム名を引数に渡します
 
 # ジェネレータ式１
+# 1~10まで2つ数字を飛ばしてiが出力される(1, 3, 5, 7, 9)
 gene1 = (i*(i+1) for i in range(1, 10, 2))
+for d in gene1:
+  print(d, end=" ")
 
 print()
-
 # ジェネレータ式２
+# enumerate() -> 0~ iを取ってくる
 list_num = [3, 11, 7, 1]
 gene2 = (i*num for i, num in enumerate(list_num))
 
-print()
+for d in gene2:
+  print(d, end=" ")
 
+print()
 # ジェネレータ式３
 list_a = ["一石", "三寒", "五臓", "七転"]
 list_b = ["二鳥", "四温", "六腑", "八倒"]
 gene3 = (a + b for a, b in zip(list_a, list_b))
 
+for i in gene3:
+  print(i, end = " ")
 print()
-
--- 2nd.semester/PythonProgramming_b/2022.07.14/04_generator/main6.py
+++ 2nd.semester/PythonProgramming_b/2022.07.14/04_generator/main6.py
@@ -2,6 +2,8 @@
 # ジェネレータ式
 doubles = (i*i for i in range(10))
 print(type(doubles))
+for d in doubles:
+  print(d, end=" ")
 
 print()
 
@@ -9,10 +11,11 @@ print()
 list_x = [10, 20, 30]
 list_y = [7, 5, 3]
 
-multi = (x*y for x, y in zip(list_x, list_y))  
-
+multi = (x*y for x, y in zip(list_x, list_y))
+for d in multi:
+  print(d, end=" ")
 print()
 
 
 doubles2 = [i*i for i in range(10)]
-
+print(doubles2)
