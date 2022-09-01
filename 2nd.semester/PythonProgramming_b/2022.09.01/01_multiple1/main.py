92d4e67df487962b31f3e5dad567ed46e0f6cc14
Masaru <hittec9609@icloud.com>
2022-09-01 13:57:25 +0900

    2h

---
+++ 2nd.semester/PythonProgramming_b/2022.09.01/01_multiple1/main.py
@@ -0,0 +1,9 @@
+from square import Square
+
+square_1 = Square(5)
+
+# diamondクラスの関数(辺の1周の長さを求める)
+print(square_1.culc_perimeter())
+
+# rectangleクラスの関数(面積を求める)
+print(square_1.culc_area())
--- 2nd.semester/PythonProgramming_b/2022.09.01/01_multiple1/square.py
+++ 2nd.semester/PythonProgramming_b/2022.09.01/01_multiple1/square.py
@@ -3,10 +3,10 @@ from rectangle import Rectangle
 
 # 正方形クラス
 class Square(Diamond, Rectangle):
-    
+
     # コンストラクタで１辺の長さをもらい、
-    # ひし形クラス、平行四辺形クラスにそれぞれ設定する
+    # ひし形クラス、長方形クラスにそれぞれ設定する
     def __init__(self, length):
-        pass
-
-
+        self.set_side_length(length)
+        self.set_height(length)
+        self.set_width(length)
