ab4e23622080fbbb9faf7610477965dae0c0897a
Masaru <hittec9609@icloud.com>
2022-09-06 16:04:40 +0900

    8h

--- 2nd.semester/ComputerScience/2022.09.06/case1.py
+++ 2nd.semester/ComputerScience/2022.09.06/case1.py
@@ -7,17 +7,15 @@ while j < n-1:
   m = n - 1
 
   while j < m:
-    if dat[m] > dat[m-1]:
+    if dat[m-1] <= dat[m]:
+      m = m - 1
+
+    elif dat[m-1] > dat[m]:
       w = dat[m]
       dat[m] = dat[m-1]
       dat[m-1] = w
       m = m - 1
       count += 1
-
-    elif dat[m] <= dat[m-1]:
-      m = m - 1
-
-
 
   j = j + 1
 
