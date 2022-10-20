be4408049e54146a9002bb17b0b18ce789a1f33c
Masaru <hittec9609@icloud.com>
2022-10-20 15:07:14 +0900

    python programming

--- term.3/GamePrograming/2022.10.20/03_thread/thread1.py
+++ term.3/GamePrograming/2022.10.20/03_thread/thread1.py
@@ -2,16 +2,18 @@ from logging import getLogger, config
 import sys
 import yaml
 import time
-#定義ファイルを使ったloggingの設定
-with open('logconfig1.yaml', 'r') as yml:
-    conf =  yaml.safe_load(yml)
+import threading
+
+# 定義ファイルを使ったloggingの設定
+with open("logconfig1.yaml", "r") as yml:
+    conf = yaml.safe_load(yml)
 config.dictConfig(conf)
-#ロガーの生成
+# ロガーの生成
 logger = getLogger("main")
 
 # 数字入力関数
 def input_num():
-    
+
     ch = "a"
     while len(ch) != 0:
         try:
@@ -22,18 +24,24 @@ def input_num():
             print(f"{num} が入力されました！")
             # ログの出力
             logger.info(f"入力された数字[{num}]")
-            
+
         except ValueError as e:
             # ログの出力
             logger.error(e)
     print("処理を終了します")
 
+
+# input_num関数を対象にして、スレッドのインスタンスを作成
+input_thread = threading.Thread(target=input_num)
+# スレッドの開始
+input_thread.start()
+
+
 # 最大100回の繰り返し
-for _ in range(100) :
+for _ in range(100):
     # 数字入力関数の呼び出し
-    input_num()
+    # input_num()
     # ログの出力
-    logger.info('ループ処理の最後')
+    logger.info("ループ処理の最後")
     # タイマー（一定時間待つ）処理（１秒を指定）
     time.sleep(1)
-
